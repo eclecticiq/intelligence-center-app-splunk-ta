81a42a409f496815b3c099d08afe0caadc18a8f4
puja-sacumen <puja.sharma@sacumen.com>
2022-07-14 15:22:40 +0530

    Bugfix in create sighting and lookup observables.

--- TA-eclecticiq/bin/create_sighting.py
+++ TA-eclecticiq/bin/create_sighting.py
@@ -8,7 +8,6 @@ import sys
 import traceback
 
 import requests
-import splunk
 from splunk.clilib import cli_common as cli
 from splunk.persistconn.application import PersistentServerConnectionApplication
 
@@ -68,6 +67,7 @@ from constants.sighting_right_click import (
     VALUE,
 )  # pylint: disable=C0413
 from utils.formatters import format_proxy_uri  # pylint: disable=C0413
+from validator.logger_manager import setup_logging  # pylint: disable=C0413
 
 if sys.platform == "win32":
     import msvcrt
@@ -78,23 +78,7 @@ if sys.platform == "win32":
     msvcrt.setmode(sys.stderr.fileno(), os.O_BINARY)  # pylint: disable=E1101
 
 # Setup logging
-logger = logging.getLogger("splunk.eiq")
-SPLUNK_HOME = os.environ["SPLUNK_HOME"]
-
-LOGGING_DEFAULT_CONFIG_FILE = os.path.join(SPLUNK_HOME, "etc", "log.cfg")
-LOGGING_LOCAL_CONFIG_FILE = os.path.join(SPLUNK_HOME, "etc", "log-local.cfg")
-LOGGING_STANZA_NAME = "python"
-LOGGING_FILE_NAME = "ta_eclecticiq_create_sighting.log"
-BASE_LOG_PATH = os.path.join("var", "log", "splunk")
-LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
-splunk_log_handler = logging.handlers.RotatingFileHandler(
-    os.path.join(SPLUNK_HOME, BASE_LOG_PATH, LOGGING_FILE_NAME), mode="a"
-)
-splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
-logger.addHandler(splunk_log_handler)
-splunk.setupSplunkLogger(
-    logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME
-)
+logger = setup_logging("ta_eclecticiq_create_sighting", log_level=logging.DEBUG)
 
 
 class Send(PersistentServerConnectionApplication):  # type: ignore
@@ -249,6 +233,7 @@ class Send(PersistentServerConnectionApplication):  # type: ignore
             for stanza, fields in localsettingsconf.items():
                 if stanza == PROXY:
                     return fields
+        return {}
 
     @staticmethod
     def get_response_content(response):
